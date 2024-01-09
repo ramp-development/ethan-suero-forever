9b59b4bf11721c40ca19e14d5e36c42b4ddd1140
g4knr <85592362+g4knr@users.noreply.github.com>
2024-01-09 12:16:07 +0000

    Added location slider

---
+++ src/pages/home/index.ts
@@ -0,0 +1,6 @@
+import { locations } from './locations';
+
+export const home = () => {
+  console.log('home');
+  locations();
+};
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,9 +1,13 @@
 import { about } from './about';
+import { home } from './home';
 
 export const pages = () => {
   const { pathname } = window.location;
 
   switch (pathname) {
+    case '/':
+      home();
+      break;
     case '/about':
       about();
       break;
