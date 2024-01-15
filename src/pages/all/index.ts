26273ef89ce65783e16f4afa9d0c55c450f472ae
g4knr <85592362+g4knr@users.noreply.github.com>
2024-01-15 15:52:21 +0000

    Added swiper functionality

--- src/pages/all/index.ts
+++ src/pages/all/index.ts
@@ -1,54 +1,15 @@
 import { autoNumbers } from './autoNumbers';
 import { locations } from './locations';
-import { newWho } from './newWho';
 import { staggerText } from './staggerText';
 import { transitions } from './transitions';
-import { whoComponent } from './whoComponent';
-import { whoSwiper } from './whoSwiper';
+import { who } from './who';
 
 export const all = () => {
   console.log('all');
 
-  try {
-    transitions();
-  } catch (error) {
-    console.error('transitions failed', error);
-  }
-
-  try {
-    autoNumbers();
-  } catch (error) {
-    console.error('autoNumbers failed', error);
-  }
-
-  try {
-    staggerText();
-  } catch (error) {
-    console.error('staggerText failed', error);
-  }
-
-  try {
-    locations();
-  } catch (error) {
-    console.error('locations failed', error);
-  }
-
-  setTimeout(() => {
-    console.log('running newWho');
-    newWho();
-  }, 1000);
-
-  // try {
-  //   whoSwiper();
-  // } catch (error) {
-  //   console.error('whoSwiper failed', error);
-  // }
-
-  // try {
-  //   whoComponent();
-  // } catch (error) {
-  //   console.error('whoComponent failed', error);
-  // }
-
-  console.log('all end');
+  transitions();
+  autoNumbers();
+  staggerText();
+  locations();
+  who();
 };
